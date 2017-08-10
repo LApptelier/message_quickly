616297b45e4dfc27e6c0c36118dfd5274660dc2a
Edouard Brèthes <edouard.brethes@lapptelier.com>
2017-08-10 23:42:43 +0100

    handling simple sharing button

--- lib/message_quickly/messaging/element_share_button.rb
+++ lib/message_quickly/messaging/element_share_button.rb
@@ -2,15 +2,13 @@ module MessageQuickly
   module Messaging
     class ElementShareButton < Button
 
-      attr_accessor :payload
-
       def initialize(params = {})
         params['type'] ||= 'element_share'
         super(params)
       end
 
       def to_hash
-        { type: type, payload: payload }
+        { type: type }
       end
 
     end
@@ -19,20 +17,4 @@ end
 
 # {
 #   "type":"element_share",
-#   "payload":{
-#     "template_type":"generic",
-#       "elements":[
-#         {
-#           "title":"Breaking News: Record Thunderstorms",
-#           "subtitle":"The local area is due for record thunderstorms over the weekend.",
-#           "image_url":"https://thechangreport.com/img/lightning.png",
-#           "buttons":[
-#             {
-#               "type":"element_share"
-#             }
-#           ]
-#         }
-#       ]
-#     }
-#   }
 # }
